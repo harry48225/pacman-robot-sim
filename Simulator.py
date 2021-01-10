57716dd3f4f8e186f863fcbdf314ab385d67ada1
Harry <harrybest800@gmail.com>
2021-01-10 11:34:28 +0000

    Added truffle mechanics

--- Simulator.py
+++ Simulator.py
@@ -9,6 +9,8 @@ from termcolor import colored
 
 class Robot(object):
 
+    __STARTING_POS = [1,1]
+
     __SPRITE_UP = "^"
     __SPRITE_DOWN = "V"
     __SPRITE_LEFT = ">"
@@ -16,19 +18,22 @@ class Robot(object):
 
     __GHOST_LETTERS = ["B", "R", "O", "P"]
 
+    __TRUFFLE_SPRITE = "t"
+
     # ONLY ACCESS THE ROBOTS ATTRIBUTES EXPOSED BY GET METHODS
     def __init__(self):
         self.__MAP = self.__load_map()
 
-        self.__pos = [1,1] # First coordinate is the row number, the second is the column number
+        self.__pos = self.__STARTING_POS # First coordinate is the row number, the second is the column number
 
         self.__direction = [1, 0]
         self.__sprite = self.__SPRITE_DOWN
 
+        self.__holding_truffle = False
+
 
     def __vector_addition(self, vec1, vec2):
         return [vec1[i] + vec2[i] for i in range(2)]
-
 
     def __load_map(self):
 
@@ -44,10 +49,10 @@ class Robot(object):
             self.__sprite = self.__SPRITE_DOWN
         
         elif self.__direction == [0, 1]:
-            self.__sprite = self.__SPRITE_RIGHT
+            self.__sprite = self.__SPRITE_LEFT
         
         elif self.__direction == [0, -1]:
-            self.__sprite = self.__SPRITE_LEFT
+            self.__sprite = self.__SPRITE_RIGHT
         
         elif self.__direction == [-1, 0]:
             self.__sprite = self.__SPRITE_UP
@@ -62,6 +67,15 @@ class Robot(object):
             return True
         return False
 
+    def is_truffle_ahead(self):
+        '''Returns true if there is a truffle in front of the robot'''
+        ahead_coordinates = self.__vector_addition(self.__pos, self.__direction)
+
+        if self.__MAP[ahead_coordinates[0]][ahead_coordinates[1]] == self.__TRUFFLE_SPRITE:
+            return True
+        return False
+
+
     def move_forward(self):
         '''Moves the robot one unit forward'''
 
@@ -72,18 +86,40 @@ class Robot(object):
             if self.__MAP[self.__pos[0]][self.__pos[1]] in self.__GHOST_LETTERS: # If we've walked into a ghost
                 print("ON GHOST!!!")
 
-    def turn_right(self):
-        '''Rotates the robot right by 90 degrees'''
+    def turn_left(self):
+        '''Rotates the robot left by 90 degrees'''
         self.__direction = [-self.__direction[1], self.__direction[0]]
         
         self.__update_sprite()
 
-    def turn_left(self):
-        '''Rotates the robot left by 90 degrees'''
+    def turn_right(self):
+        '''Rotates the robot right by 90 degrees'''
         self.__direction = [self.__direction[1], -self.__direction[0]]
 
         self.__update_sprite()
 
+    def pick_up_truffle(self):
+        ''' Picks up a truffle if it's infront of the robot, and the robot has space for it'''
+
+        if not self.__holding_truffle and self.is_truffle_ahead():
+            self.__holding_truffle == True
+
+            truffle_location = self.__vector_addition(self.__pos, self.__direction)
+
+            self.__MAP[truffle_location[0]][truffle_location[1]] = " " # Remove the truffle from the map
+
+            print("Picked up truggle")
+
+    def drop_truffle(self):
+        ''' Drops a truffle at the robot's current location '''
+
+        print(self.__holding_truffle)
+        if self.__holding_truffle:
+            self.__MAP[self.__pos[0]][self.__pos[1]] = "t" # Drop the truffle
+            self.__holding_truffle = False
+        
+            if self.__pos == self.__STARTING_POS:
+                print("DELIVERED TRUFFLE!") 
 
     def print_map(self):
         '''Prints the current state of the map to the terminal'''
@@ -96,7 +132,7 @@ class Robot(object):
 
                 if char == "#":
                     colour = 'white'
-                elif char == "t":
+                elif char == self.__TRUFFLE_SPRITE:
                     colour = 'grey'
                     on_colour = 'on_white'
                 elif char == 'S':
--- test.py
+++ test.py
@@ -2,14 +2,24 @@ from Simulator import *
 
 r = Robot()
 
-r.print_map()
-r.move_forward()
-r.print_map()
-r.turn_left()
-r.print_map()
-r.move_forward()
-r.print_map()
-r.turn_right()
-r.print_map()
-r.move_forward()
+
+instructions = list("fffflfflfffllfffrffrffff")
+
+
+for instruction in instructions:
+
+    if instruction == "f":
+        r.move_forward()
+    elif instruction == "l":
+        r.turn_left()
+    elif instruction == "r":
+        r.turn_right()
+
+    r.print_map()
+
+    if (r.is_truffle_ahead()):
+        print("truffle ahead")
+        r.pick_up_truffle()
+
+r.drop_truffle()
 r.print_map()
